5a4bf92e79bea43dd99b860ebe600b2b028435d1
vinodthirumeni <vinodparamasivam2611@gmail.com>
2020-10-23 11:05:10 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyCl9POn6SusSMEd4nkfYv3_CL5UxzCgtLs",
+  authDomain: "spotify-clone-3b808.firebaseapp.com",
+  databaseURL: "https://spotify-clone-3b808.firebaseio.com",
+  projectId: "spotify-clone-3b808",
+  storageBucket: "spotify-clone-3b808.appspot.com",
+  messagingSenderId: "260119063792",
+  appId: "1:260119063792:web:d3d762e10fa00901abe635",
+  measurementId: "G-PFVX8PN2JE"
+};
+
+
+// If firebase expired
+// https://console.firebase.google.com/u/0/project/spotify-clone-3b808/firestore/rules
